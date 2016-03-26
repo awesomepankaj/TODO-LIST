36b7db152338fe303de69989829a9642cb482fc7
awesomepankaj <pankaj@mindgrep.com>
2016-03-26 11:45:43 +0530

    complete todo app with html, react, redux

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -7,7 +7,7 @@ const todo = (state, action) => {
         completed: false
       }
     case 'TOGGLE_TODO': 
-      if (state.id !== action._id) {
+      if (state.id !== action.id) {
         return state 
       } 
 
