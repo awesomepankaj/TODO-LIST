43b08b6b0ea44b46af1eab95fb429e2dbcffaf7a
awesomepankaj <pankaj@mindgrep.com>
2016-03-27 20:30:14 +0530

    add icons for todo and done task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,23 @@
 import React, { PropTypes } from 'react'
 import ListItem from 'material-ui/lib/lists/list-item';
+import styles from 'material-ui/lib/styles';
+import Avatar from 'material-ui/lib/avatar';
+import ActionAssignment from 'material-ui/lib/svg-icons/action/assignment';
+import IconButton from 'material-ui/lib/icon-button';
+import Done from 'material-ui/lib/svg-icons/action/done';
 
+var colors = styles.Colors
 const Todo = ({onClick, completed, text}) => (
   <ListItem 
-    onClick={onClick}
+    leftAvatar={<Avatar icon={<ActionAssignment />} backgroundColor={colors.blue500} />}
+    rightIconButton={
+      <IconButton 
+        tooltip="Done" 
+        onClick={onClick}
+      >
+        <Done />
+      </IconButton>
+    }
     style={{
       textDecoration: completed ? 'line-through' : 'none'
     }}
